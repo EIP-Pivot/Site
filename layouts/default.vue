8fb9c0ead8e616d18611815bdd771d86d11272cd
ErinGrouille <erin.grouille@epitech.eu>
2021-10-18 14:47:23 +0200

    Modif name and links

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,6 +32,15 @@ html {
   padding: 0px;
 }
 
+.article {
+  align-content: center;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 100px;
+  margin-bottom: 30px;
+  color: #258c9e;
+}
+
 .button--blue {
   display: inline-block;
   border-radius: 4px;
@@ -41,6 +50,8 @@ html {
   padding: 10px 30px;
   margin-left: 5px;
   margin-right: 5px;
+  margin-top: 3px;
+  margin-bottom: 3px;
 }
 
 .button--blue:hover {
@@ -57,10 +68,49 @@ html {
   padding: 10px 30px;
   margin-left: 5px;
   margin-right: 5px;
+  margin-top: 3px;
+  margin-bottom: 3px;
 }
 
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
 }
+
+.link--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.link--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+.link--blue {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #258c9e;
+  color: #258c9e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.link--blue:hover {
+  color: #fff;
+  background-color: #258c9e;
+}
+
 </style>
