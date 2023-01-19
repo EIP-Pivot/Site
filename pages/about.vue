56afe5d00ee65d783999986cad4336bd91cf8233
ErinGrouille <erin.grouille@epitech.eu>
2023-01-19 03:35:27 +0100

    added screenshots and structure to about
    still needs filling

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="container">
     <div class="contact">
+
       <div class="article">
+        <div class="article_title">
+        Mais y'a quoi dans Pivot Engine ?
+        </div>
+        lorem ipsum
+      </div>
+
+      <div class="spacing"></div>
+
+      <div class="article">
+        <div class="article_title">
+        L'Editeur de scènes Pivot
+        </div>
+        <img class="editeur" src="/2023/pivot/editeur_1.png" width=95% />
+        <br>
+        lorem ipsum
+      </div>
+      
+      <div class="spacing"></div>
+      
+      <div class="article">
+        <div class="article_title">
+        Le Langage PivotScript
+        </div>
+        <img class="pivotscript" src="/2023/pivot/pivotscript.png" />
+        <br>
+        lorem ipsum
+      </div>
+
+      <div class="spacing"></div>
+
+      <div class="article">
+        <div class="article_title">
+        Et si je souhaite en savoir plus ou participer au projet ?
+        </div>
         <p>
-          Contacter l'équipe Pivot par mail:
+          Contactez-nous par mail:
           <br><br>
           <a href="mailto:pivot_2023@labeip.epitech.eu">pivot_2023@labeip.epitech.eu</a>
         </p>
       </div>
+
     </div>
   </div>
 </template>
